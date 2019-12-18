2f329501d3f5ff386a8f616d98f16e4003e3ad2b
이한영 <dev@lhy.kr>
2019-12-18 14:23:56 +0900

    vote기능 구현

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -28,9 +28,33 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
+    # Template: /polls/templates/polls/results.html
+
+    # question_id에 해당하는 Question을 보여주고
+    # 해당 Question에 연결된 Choice목록을 보여주며
+    #  보여줄때, votes값도 같이 보여주도록 함
+
     response = "You're looking at the results of question {}"
     return HttpResponse(response.format(question_id))
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    # 특정 Question에 해당하는
+    # 특정 Choice의 votes를 1늘리기
+    # 이후 특정 Question에 해당하는 results페이지로 이동
+    if request.method == 'POST':
+        # GET parameter로 전달된 question_id에 해당하는 Question객체
+        question = get_object_or_404(Question, pk=question_id)
+
+        # form의 POST요청으로 전달된 Choice의 pk값
+        choice_pk = request.POST['choice']
+
+        # 전달받은 Choice pk에 해당하는 Choice객체
+        choice = get_object_or_404(Choice, pk=choice_pk)
+
+        # 선택된 Choice의 votes값을 1증가
+        choice.votes += 1
+        choice.save()
+
+        # results페이지로 이동
+        return redirect('polls:results', question_id=question_id)
